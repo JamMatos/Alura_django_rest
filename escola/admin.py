211039803c0e0516c4cc7aca52bf645e8604c03f
Jamil Monteiro <jamil.matos@agenciadedados.com>
2025-07-24 14:47:26 -0300

    Terminando aula 02

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from escola.models import Estudante, Curso
 
 # Register your models here.
+class Estudantes(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email','cpf','data_nascimento','celular')
+    list_display_links = ('id', 'nome',)
+    list_per_page = 20
+    search_fields = ('nome',)
+
+admin.site.register(Estudante, Estudantes)
+
+class Cursos(admin.ModelAdmin):
+    list_display = ('id', 'code', 'descricao')
+    list_display_links = ('id', 'code',)
+    search_fields = ('code',)
+
+admin.site.register(Curso, Cursos)
