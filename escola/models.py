a84eadf79c8b7e703d3f58905edddd76b2c207b7
Jamil Monteiro <jamil.matos@agenciadedados.com>
2025-07-26 09:20:54 -0300

    Atualizando projeto

--- escola/models.py
+++ escola/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
 class Estudante(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(max_length=30, blank=False)
-    cpf = models.CharField(max_length=11)
+    cpf = models.CharField(max_length=11, unique=True)
     data_nascimento = models.DateField()
     celular = models.CharField(max_length=14)
 
@@ -18,7 +19,7 @@ class Curso(models.Model):
         ('A', 'Avançado'),
     ]
 
-    code = models.CharField(max_length=10)
+    code = models.CharField(max_length=10, unique=True, validators=[MinLengthValidator(3)])
     descricao = models.CharField(max_length=100, blank=False)
     nivel = models.CharField(max_length=1, choices=NIVEL_ESCOLHA, default='B', blank=False, null=False)
 
---
+++ escola/validators.py
@@ -0,0 +1,13 @@
+import re
+
+def cpf_invalido(cpf):
+    return len(cpf) != 11
+
+def nome_invalido(nome):
+    return not nome.isalpha()
+
+def celular_invalido(celular):
+    modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
+    resposta = re.findall(modelo,celular)
+    print(resposta)
+    return not resposta
